a32c782650c4cc0adf51250fe249167d7246c59b
makefu <github@syntax-fehler.de>
2019-07-11 17:55:20 +0200

    default.nix: also provide storage.json

--- default.nix
+++ default.nix
@@ -2,6 +2,10 @@
 mkYarnPackage {
   name = "node-light";
   src = ./.;
+  postInstall = ''
+    install -D -t $out/share storage.json
+  '';
+
   packageJSON = ./package.json;
   yarnLock = ./yarn.lock;
   yarnNix = ./yarn.nix;
